d7a7a8f90ec3a42c4c6574543ce915fe0d3e53c0
Daniel Barrow <danbarrow257@gmail.com>
2024-03-11 20:25:56 -0500

    Add options to FullAnalysis.sh script to overwrite defaults and allows for just parts of full analysis to be run

--- Scripts/FullAnalysis.sh
+++ Scripts/FullAnalysis.sh
@@ -13,7 +13,64 @@ XSEC_CONFIG="./Configs/xsec_config.txt"
 SLICE_CONFIG="./Configs/tutorial_slice_config.txt"
 SYST_CONFIG="./Configs/systcalc.conf"
 
-#./Scripts/ReprocessNTuples.sh ${PROCESSED_NTUPLE_DIR} ${PELEE_NTUPLE_CONFIG}
-#./Scripts/UniverseMaker.sh ${FPM_CONFIG} ${BIN_CONFIG} ${UNIV_OUTPUT_FILE} 
-#./Scripts/PlotSlices.sh ${FPM_CONFIG} ${SYST_CONFIG} ${SLICE_CONFIG} ${UNIV_OUTPUT_FILE} ${MEASUREMENT_OUTPUT_FILE}
-./Scripts/Unfolder.sh ${XSEC_CONFIG} ${SLICE_CONFIG} ${UNF_MEAS_OUTPUT_FILE}
+POSITIONAL_ARGS=()
+
+DO_NTUPLE=0
+DO_UNIVMAKE=0
+DO_PLOT=0
+DO_UNFOLD=0
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+      -x|--xsec_config)
+	  XSEC_CONFIG="$2"
+	  shift # past argument
+	  shift # past value
+	  ;;
+      -o|--output_file)
+	  UNF_MEAS_OUTPUT_FILE="$2"
+          shift # past argument
+          shift # past value
+          ;;
+      -n|--ntuple)
+	  DO_NTUPLE=1
+	  shift # past argument
+	  ;;
+      -m|--univmake)
+	  DO_UNIVMAKE=1
+	  shift # past argument
+	  ;;
+      -p|--plot)
+	  DO_PLOT=1
+	  shift # past argument
+	  ;;
+      -u|--unfold)
+	  DO_UNFOLD=1
+	  shift # past argument
+	  ;;
+      -*|--*)
+	  echo "Unknown option $1"
+	  exit 1
+	  ;;
+      *)
+	  POSITIONAL_ARGS+=("$1") # save positional arg
+	  shift # past argument
+	  ;;
+  esac
+done
+
+if [[ $DO_NTUPLE -eq 1 ]]; then
+    ./Scripts/ReprocessNTuples.sh ${PROCESSED_NTUPLE_DIR} ${PELEE_NTUPLE_CONFIG}
+fi
+
+if [[ $DO_UNIVMAKE -eq 1 ]]; then
+    ./Scripts/UniverseMaker.sh ${FPM_CONFIG} ${BIN_CONFIG} ${UNIV_OUTPUT_FILE}
+fi
+
+if [[ $DO_PLOT -eq 1 ]]; then
+    ./Scripts/PlotSlices.sh ${FPM_CONFIG} ${SYST_CONFIG} ${SLICE_CONFIG} ${UNIV_OUTPUT_FILE} ${MEASUREMENT_OUTPUT_FILE}
+fi
+
+if [[ $DO_UNFOLD -eq 1 ]]; then
+    ./Scripts/Unfolder.sh ${XSEC_CONFIG} ${SLICE_CONFIG} ${UNF_MEAS_OUTPUT_FILE}
+fi
